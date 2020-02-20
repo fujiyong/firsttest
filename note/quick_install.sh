f23026bdbaf03e1548dc0950f52de3cffd8289b4
fujiyong <fujiyong@yctctech.com>
2020-02-20 15:12:15 +0800

    daily modify

--- note/quick_install.sh
+++ note/quick_install.sh
@@ -308,8 +308,9 @@ install_items "wget" "curl"
 ############################### git ###########################################
 install_item git
 #####git configure
-git config --global alias.co checkout
+git config --global alias.st status
 git config --global alias.ci commit
+git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.unstage 'reset HEAD'
 git config --global alias.last 'log -1'
